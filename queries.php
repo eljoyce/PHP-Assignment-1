12e2ca9dc7c2584625329fe3f639f681f1ce9f18
Ellen <32269826+eljoyce@users.noreply.github.com>
2018-02-06 23:57:10 +0000

    Add files via upload

--- queries.php
+++ queries.php
@@ -3,7 +3,7 @@
 
 <head>
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-<title>Untitled 1</title>
+<title>Queries</title>
 <style type="text/css">
 body {
   background-image: url("knitting%20top%20image.png");
@@ -38,14 +38,8 @@ body {
 	color: #c00;
 	background-color: #fff; }
 
-.auto-style3 {
-	font-size: xx-large;
-	text-align: center;
-}
-
-.auto-style4 {
-	text-align: center;
-	font-size: xx-large;
+.auto-style1 {
+	font-size: large;
 }
 
 </style>
@@ -73,6 +67,58 @@ body {
 		<li class="auto-style7" style="width: 150px"><a href="reports.php">Reports</a></li>
 	</ul>
 </div>
+<?php
+
+include("detail.php");
+
+//Three table join
+$sql = "SELECT  * FROM membersRegisteredForEvents;";
+$result = $db->query($sql);
+$row = mysqli_fetch_assoc($result);
+$num_results = mysqli_num_rows ($result);
+echo "<p><strong>Three Table Join:</strong></p>";
+for($i = 0; $i < $num_results; $i++){
+  	$eventID = $row['eventID'] ;
+  	$studentNo = $row['studentID'] ;
+  	$signupID = $row['signUpID'] ;
+  	$sql2 = "select firstName, surname from members where studentID like '%".$studentNo."%'"; 
+	$result2 = $db->query($sql2);
+	$row2 = mysqli_fetch_assoc($result2);	  	
+	$num_results2 = mysqli_num_rows ($result2);
+	$sql3 = "select eventName from events where eventID like '%".$eventID."%'"; 
+	$result3 = $db->query($sql3);
+	$row3 = mysqli_fetch_assoc($result3);	  	
+	$num_results3 = mysqli_num_rows ($result3);
+
+	echo "<br/>Student Name: ".$row2['firstName']." ".$row2['surname'];
+	echo "<br/>Event Name: ".$row3['eventName'];
+	echo "<p></p>";
+	$row = mysqli_fetch_assoc($result);
+
+}
+ //Summary Function
+$sql = "SELECT eventID, eventName FROM events;";
+$result = $db->query($sql);
+$row = mysqli_fetch_assoc($result);
+$num_results = mysqli_num_rows ($result);
+
+echo "<p><strong>Summary functions:</strong></p>";
+for($i = 0; $i < $num_results; $i++){
+  	$eventID = $row['eventID'] ;
+  	$sql2 = "select COUNT(studentID) from membersRegisteredForEvents where eventID like '%".$eventID."%'"; 
+	$result2 = $db->query($sql2);
+	$row2 = mysqli_fetch_assoc($result2);	  	
+	$num_results2 = mysqli_num_rows ($result2);
+	
+	echo "<br/>Event ID: ".$row['eventID'];
+	echo "<br/>Event Name: ".$row['eventName'];
+	echo "<br/>No of People Attending: ".$row2['COUNT(studentID)'];
+	echo "<p></p>";
+	$row = mysqli_fetch_assoc($result);
+
+}
+ 	
+?>
 
 </body>
 
